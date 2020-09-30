6606682a4081bb028fde174a4ccb7439e0078130
Deanna0202 <40579862+de08oei@users.noreply.github.com>
2020-09-30 16:15:21 -0700

    Add files via upload

---
+++ MetroSim.h
@@ -0,0 +1,31 @@
+/*
+ *
+ */
+
+#ifndef _METROSIM_H_
+#define _METROSIM_H_
+
+#include <vector>
+#include "Passenger.h"
+
+// Put any other necessary includes here
+
+
+// Put any other structs you need here
+
+class MetroSim
+{
+public:
+
+private:
+    
+    struct station {
+        string name;
+        int stationNum;
+        vector<Passenger> waiting;
+        bool trainPresent;
+    };
+
+};
+
+#endif
---
+++ Passenger.cpp
@@ -0,0 +1,15 @@
+/*
+ *
+ */
+
+#include <iostream>
+#include <string>
+
+/*
+ * Remember to write a complete function contract here!
+ */
+void Passenger::print()
+{
+        // TODO: print the passenger information to cout
+        //       as described in the homework specification
+}
